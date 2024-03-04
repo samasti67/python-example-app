b1cc8786b5668d77a08148635e541cf64c283cee
Samasti <samasti2007@gmail.com>
2024-03-04 14:29:23 +0100

    add HTML to my app

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('hello.html')
 
 @app.route('/about')
 def about():
