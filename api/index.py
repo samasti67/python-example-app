8dcd565b68c8c8c799f051bbf498f48b6f292fc5
Samasti <samasti2007@gmail.com>
2024-03-04 14:41:45 +0100

    I broke my puthon code!

--- api/index.py
+++ api/index.py
@@ -1,3 +1,6 @@
+'''
+My python app
+'''
 from flask import Flask
 from flask import render_template
 
@@ -5,8 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    '''Home route'''
     return render_template('hello.html')
 
 @app.route('/about')
 def about():
+    '''About page'''
     return 'hullabulla'
